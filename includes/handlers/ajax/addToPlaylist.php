7233a15eede9d9e51140b30adf85700465313228
marius04popa <marius04popa@yahoo.com>
2019-01-12 20:45:14 +0200

    Update some fields

--- includes/handlers/ajax/addToPlaylist.php
+++ includes/handlers/ajax/addToPlaylist.php
@@ -6,11 +6,13 @@ if(isset($_POST['playlistId']) && isset($_POST['songId'])) {
     $playlistId = $_POST['playlistId'];
     $songId = $_POST['songId'];
 
-    $orderIdQuery = mysqli_query($connection, "SELECT MAX(playlistOrder) + 1 as playlistOrder FROM playlist_Songs WHERE playlistId='$playlistId'");
+    $orderIdQuery = mysqli_query($connection, "SELECT MAX(playlistOrder) + 1 as playlistOrder FROM playlist_songs WHERE playlistId='$playlistId'");
     $row = mysqli_fetch_array($orderIdQuery);
     $order = $row['playlistOrder'];
 
-    $query = mysqli_query($connection, "INSERT INTO playlist_Songs VALUES(null, '$songId', '$playlistId', '$order')");
+    $query = mysqli_query($connection, "INSERT INTO playlist_songs VALUES(null, '$songId', '$playlistId', '$order')");
+    var_dump($connection);
+    die();
 }
 else {
     echo "PlaylistId or songId was not passed into addToPlaylist.php";
