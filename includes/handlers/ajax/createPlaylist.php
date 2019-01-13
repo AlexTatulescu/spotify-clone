fbf7e432d6a3da084519b22b3f469991e71b71aa
Alex Tatulescu <alextatulescu97@gmail.com>
2019-01-13 16:38:51 +0200

    [master] Remove forgotten debug statement

--- includes/handlers/ajax/createPlaylist.php
+++ includes/handlers/ajax/createPlaylist.php
@@ -7,8 +7,6 @@ if (isset($_POST['name']) && isset($_POST['username'])) {
     $date = date('Y-m-d');
 
     $query = mysqli_query($connection, "INSERT INTO playlists VALUES(null, '$name', '$username', '$date')");
-    var_dump(mysqli_error($connection));
-    die();
 } else {
     echo "Name or username parameters not passed into file";
 }
